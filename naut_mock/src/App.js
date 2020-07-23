1eb00bd58f6be0fdddaef355aabdecfc89cee6ea
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-23 15:04:50 -0400

    header done

--- naut_mock/src/App.js
+++ naut_mock/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div>
       <Header />
-
+      <Landing />
 
     </div>
   );
---
+++ naut_mock/src/components/Landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing">
+                <div className="landBack">
+                    <div className="spotlight">THE NAUTILUS SPOTLIGHT</div>
+                    <div className="title">WHY THE LAWS OF PHYSICS ARE</div>
+                    <div className="title">INEVITABLE</div>
+                    <button className="readBtn">READ NOW</button>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Landing
