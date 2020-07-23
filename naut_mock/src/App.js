1de054ed2c6dd181f493064270de50eff5d5ff98
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-23 12:33:56 -0400

    links coming online

--- naut_mock/src/App.js
+++ naut_mock/src/App.js
@@ -9,14 +9,14 @@ function App() {
         <div className="Nav">
           <div className="logo"> <img className="logoI" src={require("./assets/nautilus-logo-sprite@2x.png")}/> </div>
             <div className="links">
-              <p>ISSUES</p>
-              <p>TOPICS</p>
-              <p>BLOGS</p>
-              <p>NEWSLETTERS</p>
-              <p>Facebook</p>
-              <p>twitter</p>
-              <p>Login</p>
-              <button>SUBSCRIBE</button> 
+              <p><a href="">ISSUES</a></p>
+              <p><a href="">TOPICS</a></p>
+              <p><a href="">BLOGS</a></p>
+              <p><a href="">NEWSLETTERS</a></p>
+              <p><a href=""><img src="https://img.icons8.com/material-sharp/14/000000/facebook-f.png"/></a></p>
+              <p><a href=""><img src="https://img.icons8.com/android/13/000000/twitter.png"/></a></p>
+              <p><a href="">LOGIN</a></p>
+              <button className="subBtn">SUBSCRIBE</button> 
             </div>
               
         </div>
